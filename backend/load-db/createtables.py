5e6af6f3dfc574260de255546363122382884afe
Aaron Abbott <aabbott1@ufl.edu>
2014-11-19 01:09:21 -0500

    Renamed natural gas DDL

--- backend/load-db/createtables.py
+++ backend/load-db/createtables.py
@@ -5,7 +5,7 @@ from dbutil import *
 def createTables():
     """ Populate the array with names of sql DDL files """
     for sqlFileName in ["Address.sql", "Electricity.sql", "CodeViolationsReport.sql",
-                        "FireRescueEMSResponse.sql", "Gas.sql"]:
+                        "FireRescueEMSResponse.sql", "NaturalGasReport.sql"]:
         try:
             runSqlFile("create/" + sqlFileName)  
             print "Created table '{}'".format(sqlFileName.split(".sql")[0])
