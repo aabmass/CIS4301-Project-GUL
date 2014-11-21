496d6b6994cfd18999ccb80a2470f0061c4bd58f
Aaron Abbott <aabmass@gmail.com>
2014-11-21 13:15:00 -0500

    Set up /address/<id> route

--- backend/app/views.py
+++ backend/app/views.py
@@ -4,11 +4,6 @@ from flask import json
 
 # Sets up the routes, really
 
-@app.route('/address.json')
-def someAddresses():
-    all = []
-    all.append(address.findOneByID(22))
-    all.append(address.findOneByID(24))
-    all.append(address.findOneByID(26))
-    all.append(address.findOneByID(10021))
-    return json.jsonify({"addresses": all})
+@app.route('/address/<id>')
+def someAddresses(id):
+    return json.jsonify({"addresses": address.findOneByID(id)})
