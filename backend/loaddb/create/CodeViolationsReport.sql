0d56e42cf97ba308a1bfc751bc9a4f8c9d01fafc
Aaron Abbott <aabmass@gmail.com>
2014-12-07 10:19:13 -0500

    Had to change field lengths in DDL

--- backend/loaddb/create/CodeViolationsReport.sql
+++ backend/loaddb/create/CodeViolationsReport.sql
@@ -4,6 +4,6 @@ CREATE TABLE CodeViolationsReport (
     FOREIGN KEY(address_ID) REFERENCES      Address(ID),
     violation               VARCHAR2(100)   NOT NULL,
     caseType                VARCHAR2(100)   NOT NULL,
-    inspector               VARCHAR2(20)    NOT NULL,
-    status                  VARCHAR2(20)    NOT NULL
+    inspector               VARCHAR2(30)    NOT NULL,
+    status                  VARCHAR2(30)    NOT NULL
 )
