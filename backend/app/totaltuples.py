4b8c36efd77013bc5bb3024c0c4c23bd538d60b0
Joseph McConnell <jjmdevelops@gmail.com>
2014-12-08 19:39:48 -0500

    I WANT TO TOPPLE THE TOTAL OF TUPLES

--- backend/app/totaltuples.py
+++ backend/app/totaltuples.py
@@ -8,12 +8,16 @@ from loaddb import dbutil
 def totalTuples():
 
 	totAddress = dbutil.runSQLAsDict("""SELECT Count(ID) from address""")
+	print('\n\n' + totAddress + '\n\n\n\n')
 	totElect = dbutil.runSQLAsDict("""SELECT Count(ID) from Electricityreport""")
+	print(totElect)
 	totNatGas = dbutil.runSQLAsDict("""SELECT Count(ID) from NaturalGasReport""")
+	print(totNatGas)
 	totCodeVio = dbutil.runSQLAsDict("""SELECT Count(ID) from CodeViolationsReport""")
 	totWater = dbutil.runSQLAsDict("""SELECT Count(ID) from WaterReport""")
 
-	total = totAddress + totElect + totNatGas + totCodeVio + totWater
-	numtotal = int(total)
+	total = totAddress + totElect + totNatGas + totWater
 
-	return numtotal
+
+
+	return 
